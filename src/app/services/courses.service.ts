8710b3c7debace7ad4372179e2a0f010ec7e0765
Ben Ruwel <benruwel21@gmail.com>
2021-01-16 12:26:42 +0300

    Refactor home component to use Observable data

---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,19 @@
+import { Course } from './../model/course';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CoursesService {
+    constructor(private http:HttpClient) {}
+
+    loadAllCourses(): Observable<Course[]> {
+        return this.http.get<Course[]>('/api/courses')
+        .pipe(
+            map(res => res["payload"])
+        );
+    }
+}
