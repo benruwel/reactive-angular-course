c45db1e79102890439ffba5b43f8ebeb22b07e19
Ben Ruwel <benruwel21@gmail.com>
2021-01-17 20:12:02 +0300

    Complete implementation of Angular Store Service

--- src/app/services/courses.store.ts
+++ src/app/services/courses.store.ts
@@ -1,16 +1,40 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
-import { map } from 'rxjs/operators';
+import {BehaviorSubject, Observable, throwError} from 'rxjs';
+import {catchError, map, tap} from 'rxjs/operators';
 import {Course, sortCoursesBySeqNo} from '../model/course';
+import {HttpClient} from '@angular/common/http';
+import {LoadingService} from '../loading/loading.service';
+import {MessagesService} from '../messages/messages.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesStore {
   // this is a stateful service
-  courses$: Observable<Course[]>;
+  private subject = new BehaviorSubject<Course[]>([]);
+  courses$: Observable<Course[]> = this.subject.asObservable();
 
-  constructor() {
+  constructor(private http: HttpClient,
+              private loadingService: LoadingService,
+              private messagesService: MessagesService) {
+    // happen only once, service construction time
+    this.loadAllCourses();
+  }
+
+  private loadAllCourses() {
+    const loadCourses$ = this.http.get<Course[]>('/api/courses')
+      .pipe(
+        map(response => response['payload']),
+        catchError(err => {
+          const message = 'Could not load all courses';
+          this.messagesService.showErrors(message);
+          console.log(message, err);
+          return throwError(err);
+        }),
+        tap(courses => this.subject.next(courses))
+      );
+    this.loadingService.showLoaderUntilCompleted(loadCourses$)
+      .subscribe();
   }
 
   filterByCategory(category: string): Observable<Course[]> {
